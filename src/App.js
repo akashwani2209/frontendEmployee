73e38a86073c63f82d77c5d0c9187fadd03fb9f2
akashwani2209 <akashwani2209@gmail.com>
2024-07-12 00:38:23 +0530

    App.js and App.css configured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import EmployeeForm from './EmployeeForm';
+import EmployeeTable from './EmployeeTable';
+import './App.css'; // Import your CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/">
+            <EmployeeForm />
+          </Route>
+          <Route path="/table">
+            <EmployeeTable />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
